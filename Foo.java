bdce89e7e4671465a22bb7a0f93cd5d7d93d753d
Gonzalo Baptista <gobaesis@gmail.com>
2013-04-06 01:38:47 -0400

    primer commit de la rama1

--- Foo.java
+++ Foo.java
@@ -2,6 +2,30 @@ package util;
 public class Foo {
    private String firstName;
    private String lastName;
+   private String secondName;
+   public String getFirstName() {
+     return firstName;
+   }
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+}
+   public String getSecondName() {
+      return secondName;
+   }
+   public void setSecondName(String secondName) {
+      this.secondName = secondName;
+   }
+}package util;
+public class Foo {
+   private String firstName;
+   private String lastName;
 
    public String getFirstName() {
      return firstName;
